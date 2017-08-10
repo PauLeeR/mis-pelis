cd4a28d2a433737c551f9b16fd29026bde050243
berenisser <berenisser@gmail.com>
2017-08-10 13:20:38 -0400

    imprimiendo en html

--- dist/js/script.js
+++ dist/js/script.js
@@ -12632,12 +12632,21 @@ if (typeof jQuery === 'undefined') {
 
 $(document).ready(function() {
 	var traerPeliculas = function(data){
-		var tituliPelicula, year, imagenPelicula;
+		var tituliPelicula, year, imagenPelicula, genero, duracion, director;
 		data.forEach(function(e){
 			traerPeliculas = e.show_title;
 			year = e.release_year;
 			imagenPelicula = '<img src="'+ e.poster +'">';
+			genero = e.category;
+			duracion = e.runtime;
+			director = e.director;
 			console.log(traerPeliculas + year + imagenPelicula);
+
+			$("#espacio-peliculas").append('<div class="contenedor-cada-pelicula">'+
+				'<h4>'+traerPeliculas+'</h4><span>'+year+'</span><span>'+genero+'</span>'+
+				imagenPelicula+
+				'<span class="red"><i class="fa fa-clock-o" aria-hidden="true"></i> '+duracion+' <i class="fa fa-television" aria-hidden="true"></i></span>'+
+				'</div>')
 		})
 	}
 	function llamadaAjax(){
