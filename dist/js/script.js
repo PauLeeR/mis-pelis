f47af8812ff74897987f5ce820fc8fe40023cf9e
Trinidad Fuentes <tfuentesfuentealba@gmail.com>
2017-08-11 11:31:39 -0400

    agregando favoritos

--- dist/js/script.js
+++ dist/js/script.js
@@ -12630,6 +12630,7 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
+
 $(document).ready(function() {
 	var traerPeliculas = function(data){
 		var itemSelect = [];
@@ -12638,45 +12639,98 @@ $(document).ready(function() {
 		data.forEach(function(e){
 			traerPeliculas = e.show_title;
 			year = e.release_year;
-			imagenPelicula = '<img src="'+ e.poster +'">';
+			imagenPelicula = '<img class="img-thumbnail" src="'+ e.poster +'">';
 			genero = e.category;
 			duracion = e.runtime;
 			director = e.director;
 			//console.log(traerPeliculas + year + imagenPelicula);
 
-			$("#espacio-peliculas").append('<div class="contenedor-cada-pelicula">'+
-				'<h4>'+traerPeliculas+'</h4><span>'+year+'</span><span class="categoria">'+genero+'</span>'+
-				imagenPelicula+
-				'<span class="red"><i class="fa fa-clock-o" aria-hidden="true"></i> '+duracion+' <i class="fa fa-television" aria-hidden="true"></i></span>'+
+			//pluggin que hace los rating de las estrellas
+			//$(".rateYo").rateYo({rating: e.rating, starWidth: "25px", ratedFill: "#E74C3C"});
+
+			$("#espacio-peliculas").append('<div class="contenedor-cada-pelicula row">'+
+					'<div class="col-xs-12">'+
+						'<h4 class="pull-left">'+traerPeliculas+'</h4><a class="btn-favorito pull-right ' + e.show_id + '" href="#">Add Favorite</a>'+ 
+					'</div>'+
+					'<div class="col-xs-12">'+
+						'<div class="col-xs-3 bg-imagen">'+imagenPelicula+'</div>'+ 
+						'<div class="col-xs-9 col10">'+
+							'<div class="col-xs-6">'+
+								'<span>'+year+' | </span><span>'+genero+'</span><br><span class="red">   <i class="fa fa-television" aria-hidden="true"></i> '+director+'</span><br>'+
+								'<i class="fa fa-clock-o" aria-hidden="true"></i> '+duracion+
+							'</div>'+
+							'<div class="col-xs-6 estrellitas">'+
+								'<div class="rateYo pull-right"></div>'+
+							'</div>'+
+						'</div>'+
+					'</div>'+
 				'</div>');
+
+			//Fav
+			$("#espacio-fav").prepend('<div class="item-fav fav-' + e.show_id + '"><img src="' + e.poster + '" class="img-responsive"><div class="text-fav"><p>'+e.show_title+'</p><i class="fa fa-ellipsis-h" aria-hidden="true"></i></div></div>');
 			itemSelect.push(genero);
+
 		})
 
+
+	
 		//Imprimiendo categorias en select
 		var uniqueNames = [];
 		$.each(itemSelect, function(i, el){
 		    if($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
 		});
-		var contadorSelect = 0;
-    	//console.log(uniqueNames);
+
     	for(var i = 0; i < uniqueNames.length; i++){
     		$("#espacio-categorias").append("<option value='" + i + "'>" + uniqueNames[i] + "</option>");
     	}
 		
-		if($("#espacio-categorias").val() == 1){
-		    	for(var i = 0; i < $(".categoria").length; i++ ){
-		    		if($(".categoria")[i].innerText == "Dramas"){
-		    			$("#espacio-peliculas").append($(".categoria").parent()[i])
-		    		
-		 }}
-		 }
+
+    	//Botones echos a la mala u . u (perdon)
+
+		$(".60031219").click(function(){
+    		$(".fav-60031219").addClass('mostrar');
+    	})
+    	$(".425231").click(function(){
+    		$(".fav-425231").addClass('mostrar');
+    	})
+    	$(".60025007").click(function(){
+    		$(".fav-60025007").addClass('mostrar')
+    	})
+    	$(".20159719").click(function(){
+    		$(".fav-20159719").addClass('mostrar')
+    	})  	
+    	$(".492899").click(function(){
+    		$(".fav-492899").addClass('mostrar')
+    	})
+    	$(".70039386").click(function(){
+    		$(".fav-70039386").addClass('mostrar')
+    	})
+    	$(".70242546").click(function(){
+    		$(".fav-70242546").addClass('mostrar')
+    	})
+    	$(".70206627").click(function(){
+    		$(".fav-70206627").addClass('mostrar')
+    	})
+		$(".60034580").click(function(){
+    		$(".fav-60034580").addClass('mostrar')
+    	})
+    	$(".60031289").click(function(){
+    		$(".fav-60031289").addClass('mostrar')
+    	})
+    	$(".70021646").click(function(){
+    		$(".fav-70021646").addClass('mostrar')
+    	})
+    	$(".287348").click(function(){
+    		$(".fav-287348").addClass('mostrar')
+    	})
+    	$(".70043941").click(function(){
+    		$(".fav-70043941").addClass('mostrar')
+    	})
 
 	}
-
-
 	function llamadaAjax(){
 		$.ajax({
-		url: 'https://netflixroulette.net/api/api.php?actor=Liam%20Cunningham',
+		url: 'https://netflixroulette.net/api/api.php?actor=nicole%20kidman',
 		type: 'GET',
 		dataType: 'json',
 		})
@@ -12692,5 +12746,5 @@ $(document).ready(function() {
 		});
 	}llamadaAjax()
 	
-	
+
 });
